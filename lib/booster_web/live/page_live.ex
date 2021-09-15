ffd215b8f91ce22c4ab47f0b508f93f51d462c79
Matt Silbernagel <matt@silbernagel.dev>
2021-09-15 07:58:40 -0400

    more

--- lib/booster_web/live/page_live.ex
+++ lib/booster_web/live/page_live.ex
@@ -5,7 +5,6 @@ defmodule BoosterWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    # get different brands
     %{"TableNames" => tables} = Dynamo.list_tables() |> ExAws.request!()
 
     tables =
@@ -13,7 +12,23 @@ defmodule BoosterWeb.PageLive do
       |> Enum.filter(fn t -> String.starts_with?(t, "discovery_lightyear_defaults") end)
       |> Enum.map(fn <<"discovery_lightyear_defaults_">> <> brand -> brand end)
 
-    {:ok, assign(socket, tables: tables, brand: "")}
+    {:ok, assign(socket, tables: tables, brand: "", show_add_form: false)}
+  end
+
+  def handle_event("toggleRegion", %{"region" => region, "environment" => env}, socket) do
+    key = :"#{env}_regions"
+    regions = Map.get(socket.assigns, key)
+
+    regions =
+      for r <- regions do
+        if r.name == region do
+          %{r | show: !r.show}
+        else
+          r
+        end
+      end
+
+    {:noreply, assign(socket, key, regions)}
   end
 
   def handle_event("selectBrand", %{"brand" => brand}, socket) do
@@ -30,36 +45,48 @@ defmodule BoosterWeb.PageLive do
     {:noreply, assign(socket, qa: qa_defaults, qa_regions: qa_regions, brand: brand)}
   end
 
-  defp get_regions(%{"Items" => items}),
-    do: items |> Enum.map(&Map.get(&1, "region")) |> Enum.map(&Map.get(&1, "S"))
+  def handle_event("showAddForm", %{"environment" => env}, socket) do
+    # show the add form for #{env}
+    {:noreply, assign(socket, show_add_form: true, add_for: env)}
+  end
 
-  defp get_regions(_), do: []
+  def handle_event("cancelAdd", _, socket) do
+    {:noreply, assign(socket, show_add_form: false, add_for: "")}
+  end
 
   def render(assigns) do
-    ~L"""
+    ~H"""
     <div>
-    <form>
-      <select>
-        <option selected> Select a brand </option>
-        <%= for table <- @tables do %> 
-          <option phx-click="selectBrand" phx-value-brand="<%= table %>">
-            <%= table %> 
-          </option> 
-        <% end %>
-      </select>
-    </form>
+      <form>
+        <select>
+          <option selected={@brand==""}> Select a brand </option>
+          <%= for table <- @tables do %> 
+            <option phx-click="selectBrand" phx-value-brand={table} selected={@brand==table}>
+              <%= table %> 
+            </option> 
+          <% end %>
+        </select>
+      </form>
 
-    <%= if @brand != "" do %>
-      <h1> <%= @brand %> </h1>
+      <%= if @brand != "" do %>
+        <h1> <%= @brand %> </h1>
         <div class="row">
           <div class="column">
             <h3> QA </h3>
             <%= if @qa_regions do %>
               <ul>
               <%= for r <- @qa_regions do %>
-                <li> <%= r %> </li>
+                <li phx-click="toggleRegion" phx-value-environment="qa" phx-value-region={r.name}> <%= r.name %> </li>
+                <%= if r.show do %>
+                  <ul>
+                    <%= for {k, v} <- r.data do %>
+                      <li> <%= k %> --- <%= v %> </li>
+                    <% end %>
+                  </ul>
+                <% end %>
               <% end %>
               </ul>
+              <button phx-click="showAddForm" phx-value-environment="qa"> Add QA Application </button>
             <% end %>
           </div>
           <div class="column">
@@ -70,9 +97,37 @@ defmodule BoosterWeb.PageLive do
           </div>
         </div>
 
+        <%= if @show_add_form do %>
+          <form>
             
+          </form>
+          <button phx-click="cancelAdd"> Cancel </button>
+        <% end %>
+
       <% end %>
     </div>
     """
+  end
+
+  defp get_regions(%{"Items" => items}) do
+    items
+    |> Enum.map(fn i ->
+      region = Map.get(i, "region") |> Map.get("S")
+
+      %{
+        show: false,
+        name: region,
+        data: decode_item(i)
+      }
+    end)
+  end
+
+  defp get_regions(_), do: []
+
+  defp decode_item(item) do
+    Enum.map(item, fn {k, v} ->
+      value = Enum.map(v, fn {k2, v2} -> v2 end)
+      {k, List.first(value)}
+    end)
   end
 end
