42432c09e531cfb621fe0f15a139017b750fdd64
Matt Silbernagel <matt@silbernagel.dev>
2021-09-17 07:10:42 -0400

    save data

--- lib/booster_web/live/page_live.ex
+++ lib/booster_web/live/page_live.ex
@@ -3,6 +3,8 @@ defmodule BoosterWeb.PageLive do
 
   alias ExAws.Dynamo
 
+  @table "discovery_lightyear_app"
+
   @impl true
   def mount(_params, _session, socket) do
     %{"TableNames" => tables} = Dynamo.list_tables() |> ExAws.request!()
@@ -11,6 +13,8 @@ defmodule BoosterWeb.PageLive do
       tables
       |> Enum.filter(fn t -> String.starts_with?(t, "discovery_lightyear_defaults") end)
       |> Enum.map(fn <<"discovery_lightyear_defaults_">> <> brand -> brand end)
+
+    # Get data from Dynamo
 
     countries = Req.get!("https://restcountries.eu/rest/v2/all").body
 
@@ -25,6 +29,7 @@ defmodule BoosterWeb.PageLive do
      )}
   end
 
+  @impl true
   def handle_event("toggleRegion", %{"region" => region, "environment" => env}, socket) do
     key = :"#{env}_regions"
     regions = Map.get(socket.assigns, key)
@@ -41,6 +46,7 @@ defmodule BoosterWeb.PageLive do
     {:noreply, assign(socket, key, regions)}
   end
 
+  @impl true
   def handle_event("selectBrand", %{"brand" => brand}, socket) do
     qa_defaults =
       "discovery_lightyear_defaults_#{brand}"
@@ -55,29 +61,75 @@ defmodule BoosterWeb.PageLive do
     {:noreply, assign(socket, qa: qa_defaults, qa_regions: qa_regions, brand: brand)}
   end
 
+  @impl true
   def handle_event("showAddForm", %{"environment" => env}, socket) do
     # show the add form for #{env}
     {:noreply, assign(socket, show_add_form: true, add_for: env)}
   end
 
+  @impl true
   def handle_event("cancelAdd", _, socket) do
     {:noreply, assign(socket, show_add_form: false, add_for: "")}
   end
 
-  def handle_event("addNew", form, socket) do
-    IO.inspect("Add new application for #{socket.assigns.brand}")
-    IO.inspect(form)
-    {:noreply, socket}
+  @impl true
+  def handle_event("addNew", %{"countryCode" => cc} = form, socket) do
+    rows =
+      for code <- cc do
+        # get the correct region for this country code
+        [region, country_code] = String.split(code, "-")
+        configs = socket.assigns.qa_regions
+
+        config =
+          Enum.find(configs, fn %{data: data} ->
+            Enum.find(data, fn {k, v} ->
+              if k == "regions" do
+                Enum.find(v, &(String.downcase(&1) == String.downcase(region)))
+              else
+                false
+              end
+            end)
+          end)
+
+        {_, apiUrl} = Enum.find(get_in(config, [:data]), fn {k, v} -> k == "apiUrl" end)
+        {_, dbregion} = Enum.find(get_in(config, [:data]), fn {k, v} -> k == "region" end)
+        {_, environment} = Enum.find(get_in(config, [:data]), fn {k, v} -> k == "environment" end)
+
+        {_, cognitoAppClientId} =
+          Enum.find(get_in(config, [:data]), fn {k, v} -> k == "cognitoAppClientId" end)
+
+        %{
+          "region" => dbregion,
+          "brand" => socket.assigns.brand,
+          "environment" => environment,
+          "apiUrl" => apiUrl,
+          "cognitoAppClientId" => cognitoAppClientId,
+          "countryCode" => country_code,
+          "appName" => Map.get(form, "name"),
+          "apiToken" => Map.get(form, "apiToken"),
+          "firmwareToken" => Map.get(form, "firmwareToken"),
+          "marketingProgram" => get_in(form, ["marketingProgram", "#{region}", "#{country_code}"])
+        }
+      end
+
+    for r <- rows do
+      Dynamo.put_item("discovery_lightyear_app", r) |> ExAws.request!()
+    end
+
+    {:noreply, assign(socket, show_add_form: false)}
   end
 
+  @impl true
   def handle_event("generateToken", %{"for" => field}, socket) do
     {:noreply, assign(socket, "token_#{field}": "1234abcd")}
   end
 
+  @impl true
   def handle_event("addChange", _, socket) do
     {:noreply, socket}
   end
 
+  @impl true
   def render(assigns) do
     ~H"""
     <div>
@@ -167,7 +219,7 @@ defmodule BoosterWeb.PageLive do
             <%= for country <- get_countries("asia", @countries) do %>
               <div>
                 <input type="checkbox" value={"asia-" <> Map.get(country, "alpha2Code")} name="countryCode[]"/> <%= Map.get(country, "name") %>
-                <input type="text" placeholder="Marketing Program" />
+                <input type="text" placeholder="Marketing Program" name={"marketingProgram[asia][" <> Map.get(country, "alpha2Code")<>"]"} />
               </div>
             <% end %>
 
